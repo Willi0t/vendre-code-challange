ce09cdb5f1d4f8d2b95efeaafb1a2845f96b4b4b
Willi0te <william.sinclair92@gmail.comm>
2023-07-04 00:33:01 +0200

    added pages and buttons, end of day 1

--- src/App.js
+++ src/App.js
@@ -4,59 +4,65 @@ import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardsPerPage] = useState(6);
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await fetch("https://reqres.in/api/users");
-        const data = await response.json();
+        const response1 = await fetch("https://reqres.in/api/users");
+        const data1 = await response1.json();
 
-        // Define the title mapping
-        const titleMapping = {
-          1: "UX-Designer",
-          2: "Human resources",
-          3: "CEO",
-          4: "Fullstack Developer",
-          5: "Product owner",
-          6: "Backend developer",
-          7: "Project Manager",
-          8: "Copy writer",
-          9: "Frontend developer",
-          10: "Project Manager",
-          11: "UX-Designer",
-          12: "Frontend developer",
-        };
+        const response2 = await fetch("https://reqres.in/api/users?page=2");
+        const data2 = await response2.json();
 
-        // Modify the users' data with titles
-        const modifiedUsers = data.data.map((user) => {
-          const title = titleMapping[user.id];
-          return { ...user, title };
-        });
-
-        // Fetch additional users from page 2
-        const responsePage2 = await fetch("https://reqres.in/api/users?page=2");
-        const dataPage2 = await responsePage2.json();
-        const modifiedUsersPage2 = dataPage2.data.map((user) => {
-          const title = titleMapping[user.id];
-          return { ...user, title };
-        });
-
-        // Combine the existing users with the new page of users
-        const allUsers = [...modifiedUsers, ...modifiedUsersPage2];
-        console.log(allUsers);
-        setUsers(allUsers);
+        // Combine the users from both responses
+        const allUsers = [...data1.data, ...data2.data];
+        const usersWithTitles = allUsers.map((user, index) => ({
+          ...user,
+          title: titleMapping[(index % 12) + 1],
+        }));
+        setUsers(usersWithTitles);
       } catch (error) {
         console.log("Error fetching users:", error);
       }
     };
 
+    // Define the title mapping
+    const titleMapping = {
+      1: "UX-Designer",
+      2: "Human resources",
+      3: "CEO",
+      4: "Fullstack Developer",
+      5: "Product owner",
+      6: "Backend developer",
+      7: "Project Manager",
+      8: "Copy writer",
+      9: "Frontend developer",
+      10: "Project Manager",
+      11: "UX-Designer",
+      12: "Frontend developer",
+    };
+
     fetchUsers();
   }, []);
+
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  const indexOfLastCard = currentPage * cardsPerPage;
+  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+  const currentCards = users.slice(indexOfFirstCard, indexOfLastCard);
 
   return (
     <div className="App">
       <div className="AppContainer">
-        {users.map((user) => (
+        {currentCards.map((user) => (
           <UserCard
             key={user.id}
             user={user}
@@ -64,6 +70,14 @@ function App() {
             title={user.title}
           />
         ))}
+      </div>
+      <div className="CarouselControls">
+        <button disabled={currentPage === 1} onClick={prevPage}>
+          Previous
+        </button>
+        <button disabled={indexOfLastCard >= users.length} onClick={nextPage}>
+          Next
+        </button>
       </div>
     </div>
   );
